1ba3a768d0f0568d46ea830266932f7d488bbfff
Сергей <seregabur@live.ru>
2022-03-25 20:29:05 +0300

    Добавьте GITIGNORE- и GITATTRIBUTES-файлы.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Task_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите колличество золота ");
+            string plaersGoldToEnter = Console.ReadLine();
+            int goldPlaer;
+            int crystalPrice = 12;
+            if(int.TryParse(plaersGoldToEnter, out goldPlaer))
+            {
+                Console.WriteLine("Приветствую тебя авантюрист\nв нашем магазине ты можешь купить кристаллы\nцена одного кристала = " + crystalPrice + " золота");
+                ///Console.ReadLine();
+                Console.WriteLine("Сколько кристаллов ты хочешь купить?");
+                int bayCrystals;
+                string enterPlaerCrystalsToStore = Console.ReadLine();
+                bool flag = true ;
+                while (flag)
+                {
+                    if (int.TryParse(enterPlaerCrystalsToStore, out bayCrystals))
+                    {
+
+                        int theAmountOfGoldToBayCrystals = crystalPrice * bayCrystals;
+                        goldPlaer = goldPlaer - theAmountOfGoldToBayCrystals;
+                        //по заданию задачи проверки на достаточное колличество золота не нужно делать.Поэтому вот так.
+                        Console.WriteLine("Куплено кристаллов - " + bayCrystals + ". Осталось золота - " + goldPlaer + ".");
+
+                        flag = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("ММММ это не золото попробуйте ещё раз");
+                        enterPlaerCrystalsToStore = Console.ReadLine();
+                    }
+                }
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Введёные значения не являются числом\nприложение будет закрыто");
+            }
+
+        }
+    }
+}
